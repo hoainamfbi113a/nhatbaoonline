97201e06412fe5d9a3c6ec0b0ba74e7c334c1098
Nam Nguyen <nguyeenxhoaifnam@gmail.com>
2022-11-03 09:02:50 +0700

    fix

--- js/start.js
+++ js/start.js
@@ -1,14 +1,14 @@
 /**
  * Website start here
  */
-$(window).load(function () {
+ $(window).load(function () {
 
 });
 $(document).ready(function ($) {
   var swiper = new Swiper(".mySwiper", {
     grabCursor: true,
     effect: "creative",
-    speed: 1000,
+    speed: 1500,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
@@ -17,13 +17,13 @@ $(document).ready(function ($) {
       prev: {
         shadow: true,
         origin: "left center",
-        translate: ["-5%", 0, -200],
+        translate: ["0%", 0, 0],
         rotate: [0, -100, 0],
       },
       next: {
         origin: "right center",
-        translate: ["5%", 0, -200],
-        rotate: [0, -100, 0],
+        translate: ["0%", 0, 0],
+        rotate: [0, 0, 0],
       },
     },
   });
